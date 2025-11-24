bd1f576c5b87005e992e905d74aec9134b21cb9a
MohamedTalal <mohamedtalal881@gmail.com>
2025-11-24 14:47:54 +0200

    next.config changes

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: 'export', // Add this for static export
   reactCompiler: true,
-  turbopack: {}, // silence Turbopack warning
+  turbopack: {},
+  images: {
+    unoptimized: true, // Required for static export
+  },
 };
 
 export default nextConfig;
